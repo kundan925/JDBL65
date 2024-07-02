be90e42b698e5ab2bc2c66c425a3ffafe8a9727d
kundankumar <kundankumar@localhost>
2024-07-02 05:53:41 +0530

    Asking about kid

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Kundan Here!");
+		System.out.println("How are you kid?");
 
 	}
 
